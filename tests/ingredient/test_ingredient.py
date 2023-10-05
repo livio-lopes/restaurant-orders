100bb0cf6c0ce34cf758b66134664711aeb85058
Lívio Lopes <antonio.lvio760@gmail.com>
2023-10-05 15:51:41 -0300

    test(ingredient): tests implemented for validation of class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,13 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_tomate = Ingredient("tomate")
+    ingredient_batata = Ingredient("batata")
+
+    assert ingredient_tomate == Ingredient("tomate")
+    assert ingredient_batata != Ingredient("tomate")
+    assert hash(ingredient_batata) != hash(ingredient_tomate)
+    assert hash(ingredient_tomate) == hash(Ingredient("tomate"))
+    assert repr(ingredient_tomate) == "Ingredient('tomate')"
+    assert ingredient_tomate.name == 'tomate'
+    assert len(ingredient_tomate.restrictions) == 0
