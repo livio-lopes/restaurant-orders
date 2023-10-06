7907b3d1ba47be61f86a4fabb78b466dc160cae6
Lívio Lopes <antonio.lvio760@gmail.com>
2023-10-05 22:03:05 -0300

    feat(MenuData): implemented method __init__ from class MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,21 @@
+from models.dish import Dish, Ingredient
+import csv
+
 # Req 3
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        with open(source_path) as file:
+            headers, *rows = csv.reader(file)
+            for row in rows:
+                instance = Dish(row[0], float(row[1]))
+                instance.add_ingredient_dependency(
+                    Ingredient(row[2]), int(row[3])
+                )
+                self.dishes.add(instance)
+
+
+# test = MenuData("data/menu_base_data.csv")
+# print(test.dishes)
